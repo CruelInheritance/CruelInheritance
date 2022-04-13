a9ea90ee879b824603d852bd6ba29dce4440faec
kohlglenn <kohlglenn@gmail.com>
2022-04-12 17:03:46 -0700

    open links in new tab

--- src/components/icon.js
+++ src/components/icon.js
@@ -3,7 +3,7 @@ import React from "react"
 const IconWrapper = ({ iconSrcUrl, children }) => {
   return (
     <div className="icon">
-      <a href={iconSrcUrl} className="icon">
+      <a href={iconSrcUrl} className="icon" target="_blank">
         {children}
       </a>
     </div>
